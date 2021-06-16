74c999a82e1eb9a4f0f6e945991e6ab7fa75a58c
Tarun Singh Yadav <tarunyadav148@gmail.com>
2021-06-17 01:23:34 +0530

    basic layout and navigation menu done

---
+++ script.js
@@ -0,0 +1,44 @@
+// Slide Show
+
+var slideshowCounter1 = 0;
+var slideshowCounter2 = 1;
+
+var slides = ["./pics/nitkkr.jpg","./pics/dirertor.jpg","./pics/inaug.jpeg","./pics/library.jpg","./pics/pond.jpg","./pics/nitkkr2.jpg","./pics/hostel1.jpg","./pics/hostel2.jpg","./pics/hostel3.jpg"]
+
+const slide1 = document.querySelector('#slideshowimg1');
+const slide2 = document.querySelector('#slideshowimg2');
+
+const leftbtn = document.querySelector('#left');
+leftbtn.addEventListener('click',leftShift);
+function leftShift()
+{
+    slideshowCounter1 = Math.abs(slideshowCounter1-1)%slides.length;
+    slideshowCounter2 = Math.abs(slideshowCounter2-1)%slides.length;
+    slide1.src=slides[slideshowCounter1];
+    slide2.src=slides[slideshowCounter2];
+}
+
+const rightbtn = document.querySelector('#right');
+rightbtn.addEventListener('click',rightShift);
+function rightShift()
+{
+    slideshowCounter1 = (slideshowCounter1+1)%slides.length;
+    slideshowCounter2 = (slideshowCounter2+1)%slides.length;
+    slide1.src=slides[slideshowCounter1];
+    slide2.src=slides[slideshowCounter2];
+    
+}
+    
+
+setTimeout(slideshow,5000);
+function slideshow()
+{
+    slideshowCounter1 = (slideshowCounter1+1)%slides.length;
+    slideshowCounter2 = (slideshowCounter2+1)%slides.length;
+    slide1.src=slides[slideshowCounter1];
+    slide2.src=slides[slideshowCounter2];
+    setTimeout(slideshow,5000);
+}
+
+
+//-----------
